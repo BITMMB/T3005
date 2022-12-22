d87d0f55672009b62e3cc55e57e1e6c6ca000ddf
T3000 <antonrusakov@mail.ru>
2022-12-22 19:05:39 +0300

    3.4.15

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -6,7 +6,6 @@ const defaultState = {
   oneTransfer: false,
   twoTransfer: false,
   threeTransfer: false,
-  lastreq: false,
   topFilter: 1,
 }
 
@@ -16,59 +15,51 @@ const reducer = (state = defaultState, action) => {
       return {
         ...state,
         all: false,
-        lastreq: 'allOff',
       }
 
-    case 'all':
-      if (state.all) {
-        return {
-          ...state,
-          all: false,
-          // noTransfer: false,
-          // oneTransfer: false,
-          // twoTransfer: false,
-          // threeTransfer: false,
-          // lastreq: 'all',
-        }
+    case 'allf':
+      return {
+        ...state,
+        all: false,
+        noTransfer: false,
+        oneTransfer: false,
+        twoTransfer: false,
+        threeTransfer: false,
       }
+    case 'all':
       return {
         ...state,
         all: true,
-        // noTransfer: true,
-        // oneTransfer: true,
-        // twoTransfer: true,
-        // threeTransfer: true,
-        // lastreq: 'all',
+        noTransfer: true,
+        oneTransfer: true,
+        twoTransfer: true,
+        threeTransfer: true,
       }
+
     case 'noTransfer':
       return {
         ...state,
         noTransfer: !state.noTransfer,
-        lastreq: 'noTransfer',
       }
     case 'oneTransfer':
       return {
         ...state,
         oneTransfer: !state.oneTransfer,
-        lastreq: 'oneTransfer',
       }
     case 'twoTransfer':
       return {
         ...state,
         twoTransfer: !state.twoTransfer,
-        lastreq: 'twoTransfer',
       }
     case 'threeTransfer':
       return {
         ...state,
         threeTransfer: !state.threeTransfer,
-        lastreq: 'threeTransfer',
       }
     case 'topFilter':
       return {
         ...state,
         topFilter: action.value,
-        lastreq: 'topFilter',
       }
 
     default:
